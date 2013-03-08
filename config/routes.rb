b522c3657119ba9a1dc483d1f8dbaa0a77f336b6
Jakob Pavlič <jakob.pavlic@gmail.com>
2013-03-08 04:08:18 -0800

    fotogalerija,meni.Vse zelo osnovno

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Sport::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  match '/index' => 'static#index'
   # Sample of regular route:
   #match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
