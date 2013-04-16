922af12810a3f1a59f8fe0b1d7c83a34e896ba8b
Jakob Pavlič <jakob.pavlic@gmail.com>
2013-04-16 12:53:47 +0200

    popravljene barve

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,9 +19,7 @@ Sport::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
-  
-  #color CSS for wysihtml5
-  config.assets.precompile += %w( bootstrap-wysihtml5/wysiwyg-color.css )
+
   
 
   # Defaults to Rails.root.join("public/assets")
