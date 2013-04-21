e3d99d49392fc1ec29ae52a6e8495bc9dc1096e0
Jakob Pavlič <jakob.pavlic@gmail.com>
2013-04-21 11:53:38 +0200

    sortiranje objav-najnovejše na vrhu

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   load_and_authorize_resource
   def index
     #@posts=Post.all
+    @posts=Post.find(:all, :order => 'created_at DESC')
   end
   
   def show
